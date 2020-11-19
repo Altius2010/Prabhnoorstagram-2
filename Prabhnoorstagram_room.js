34da92bdfac132c2bdbe595be100673537073b92
Altius2010 <72147694+Altius2010@users.noreply.github.com>
2020-11-19 21:25:26 +0530

    Add files via upload

---
+++ Prabhnoorstagram_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyDO-Lsavn0AmfeSCEtvP-tBTA4n1U1YJww",
+      authDomain: "prabhnoorstagram.firebaseapp.com",
+      databaseURL: "https://prabhnoorstagram.firebaseio.com",
+      projectId: "prabhnoorstagram",
+      storageBucket: "prabhnoorstagram.appspot.com",
+      messagingSenderId: "512481391020",
+      appId: "1:512481391020:web:8c7522fd6b60c3abed8a09",
+      measurementId: "G-D7Z0KGS5HM"
+};
+firebase.initializeApp(firebaseConfig);
